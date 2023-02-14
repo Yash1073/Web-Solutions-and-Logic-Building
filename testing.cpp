c2a40066290de0837c82105f77f919d9f8b4ccfe
Yash1073 <yashparmar1073@gmail.com>
2023-02-14 21:34:13 +0530

    testing

---
+++ testing.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+
+#define f           first
+#define s           second
+#define pp          pop_back
+#define vll         vector<long long>
+#define fr(i,a,n)   for (ll i=a;i<n;i++)
+#define fr1(i,n)    for(ll i=1;i<=n;i++)
+#define mpii        map<int, int>
+#define umpii       unordered_map<int, int>
+#define pii         pair<int,int>
+#define pb          push_back
+#define asort(a)    sort(a,a+n)
+#define dsort(a)    sort(a,a+n,greater<int>())
+#define vasort(v)   sort(v.begin(), v.end());
+#define vdsort(v)   sort(v.begin(), v.end(),greater<int>());
+#define umpll       unordered_map<ll,ll>
+#define pl          pair<ll,ll>
+#define MP          make_pair
+#define df(x)       x.begin(),x.end()
+#define no cout << No<< endl
+#define yes cout << YES << endl
+#define as size()
+void karma()
+{
+    cout << "hello";
+}
+int main()
+{
+    ios::sync_with_stdio(0); // Input and output clearance
+    cin.tie(0);
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        karma();
+    }
+    return 0;
+}
